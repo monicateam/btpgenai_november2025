8106a2edcbb7f87493dedca930afc9212492d5e2
Mónica Sánchez <monica.sanchez@viseo.com>
2025-12-05 11:36:15 +0000

    v2.1.6 removed hardcodes before create

--- srv/code/customertickets-beforeCreate.js
+++ srv/code/customertickets-beforeCreate.js
@@ -6,6 +6,7 @@ const LOG = cds.log('GenAI');
  * @param {cds.Request} request - User information, tenant-specific CDS model, headers and query parameters
 */
 module.exports = async function(request) {
+    const UserLoggedIn = cds.context.user;
     const customerTicketID = request.data.ID;
     if (!customerTicketID) {
         return request.reject(400, 'CustomerTicket ID is missing.');
@@ -19,10 +20,10 @@ module.exports = async function(request) {
             return request.reject(404, `CustomerMessage with ID ${customerTicketEntry} not found.`);
         }
 
-        request.data.customerId = "C004";
-        request.data.originatingCountry = "Spain";
-        request.data.sourceLanguage = "Spanish";
-        request.data.customerName = "Prueba";
+        request.data.customerId = UserLoggedIn.attr.customer_id;
+        request.data.originatingCountry = UserLoggedIn.attr.origin_country;
+        request.data.sourceLanguage = UserLoggedIn.attr.source_language;
+        request.data.customerName = UserLoggedIn.attr.customer_name;
         request.data.customerMessageID = customerTicketEntry.customerMessageID + 1;
         // await UPDATE('customerTickets').set('customerMessageID', request.data.customerMessageID);
     } catch (error) {
